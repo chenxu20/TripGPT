82b16ec71b84c274ff83da2d6905f30f73e6458a
Chen Xu <chenxu209052@gmail.com>
2024-07-20 23:15:58 +0800

    fixed routing issue

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -33,7 +33,7 @@ export default function Trips() {
                         }
                     />
                     <Route
-                        path="/calculator/:id"
+                        path="/calculator/:userId"
                         element={
                             <>
                                 <Calculator />
