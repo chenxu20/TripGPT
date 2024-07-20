1775c6285598a113b6b068b24a0efcc3e80094fa
chenxu20 <156520697+chenxu20@users.noreply.github.com>
2024-07-20 22:31:05 +0800

    Update Trips.jsx
    fixed routing issue

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -38,6 +38,8 @@ export default function Trips() {
                             <>
                                 <Calculator />
                             </>
+                        }
+                    />
                 </Routes>
             </div>
         </ItineraryContextProvider>
