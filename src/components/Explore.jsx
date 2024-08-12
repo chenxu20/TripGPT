f58dd08c63811f6c59d8c9be9188482bf09b4b23
chenxu20 <156520697+chenxu20@users.noreply.github.com>
2024-08-12 17:46:50 +0800

    Update Explore.jsx

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Map from "./Map"
 
 export default function Explore() {
     return (
         <div>
             <h1>Explore</h1>
-            <Map />
         </div>
     )
 }
